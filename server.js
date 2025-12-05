4e88a5d00c5c151e7e14ed781beb6e11167eb3a6
dayo-frontdev <dayofrontdev@gmail.com>
2025-12-05 15:40:18 +0100

    caches sample

--- server.js
+++ server.js
@@ -8,6 +8,37 @@ async function init() {
 
   app.use(express.static("./static"));
 
+  async function cache(key, ttl, slowFn) {
+    return async function cachedFn(...props) {
+      const cachhedResponse = await client.get(key);
+      if (cachhedResponse) {
+        return cachhedResponse;
+      }
+
+      const result = await slowFn(...props);
+      await client.setEx(key, ttl, slowFn);
+      return result;
+    };
+  }
+
+  async function verySlowQuery() {
+    const promise = new promise((resolve) => {
+      setTimeout(() => {
+        return new Date.toString();
+      }, 5000);
+    });
+  }
+
+  app.get("/get", async (req, res) => {
+    const cachedFn = cache("expensive-query", 10, verySlowQuery);
+    const data = cachedFn();
+
+    res.json({
+      status: "ok",
+      data,
+    });
+  });
+
   app.get("/pageview", async (req, res) => {
     const views = await client.incr("pageviews");
     res.json({
