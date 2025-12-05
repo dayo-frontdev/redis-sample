300797182f08fbbc7bc6bdcfd52d00aec3ea58ab
dayo-frontdev <dayofrontdev@gmail.com>
2025-12-05 16:58:05 +0100

    caches sample debug

--- server.js
+++ server.js
@@ -1,33 +1,34 @@
 const express = require("express");
 const redis = require("redis");
-const client = redis.createClient();
-await client.connect();
-
-async function cache(key, ttl, slowFn) {
-  return async function cachedFn(...props) {
-    const cachhedResponse = await client.get(key);
-    if (cachhedResponse) {
-      return cachhedResponse;
-    }
-
-    const result = await slowFn(...props);
-    await client.setEx(key, ttl, slowFn);
-    return result;
-  };
-}
-
-async function verySlowQuery() {
-  const promise = new promise((resolve) => {
-    setTimeout(() => {
-      resolve(new Date.toString());
-    }, 5000);
-  });
-  return promise;
-}
-
-const cachedFn = cache("expensive-query", 10, verySlowQuery);
 
 async function init() {
+  const client = redis.createClient();
+  await client.connect();
+
+  async function cache(key, ttl, slowFn) {
+    return async function cachedFn(...props) {
+      const cachhedResponse = await client.get(key);
+      if (cachhedResponse) {
+        return cachhedResponse;
+      }
+
+      const result = await slowFn(...props);
+      await client.setEx(key, ttl, slowFn);
+      return result;
+    };
+  }
+
+  async function verySlowQuery() {
+    const promise = new promise((resolve) => {
+      setTimeout(() => {
+        resolve(new Date.toString());
+      }, 5000);
+    });
+    return promise;
+  }
+
+  const cachedFn = cache("expensive-query", 10, verySlowQuery);
+
   const app = express();
 
   app.use(express.static("./static"));
